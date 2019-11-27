f8776e7891e55cedfbc9830692ce46231f4c2e54
ABID999 <abidislam49@gmail.com>
2019-11-28 01:22:00 +0600

    Changes details
    -Added template directory in base project which contains base.html which will be extended by html templates for header and footer
    - Added registration for dc for up chairman
    -later - will add form validation

--- DC/forms.py
+++ DC/forms.py
@@ -1,7 +1,7 @@
 from django import forms 
 from UpChairman import models
 
-
+# From to register new upchaiman by upchairman model
 class registerForm(forms.ModelForm):
     class Meta: 
         model = models.UpChairman
--- DC/views.py
+++ DC/views.py
@@ -2,14 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from DC.forms import registerForm
 
-
+# DC index page
 def show(request):
     return render(request, 'DChome.html')
 
-
+# dc login page
 def login_pg(request):
     return render(request, 'upcReg.html')
 
+# dc chairman register page
 def register(request):
     form = registerForm() 
 
@@ -17,7 +18,7 @@ def register(request):
         form = registerForm(request.POST)
 
         if form.is_valid():
-            form.save(commit=True)
+            form.save(commit=True) #saves from data in model
             return show(request)
         else:
             print('Invalid form')
